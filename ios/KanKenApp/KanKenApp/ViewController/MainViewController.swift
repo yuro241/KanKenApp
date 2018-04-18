4198eaa337597f87e78b1371b6526959dbe45e13
Yuichiro Tsuji <[t.yuyu.alies@gmail.com]>
2018-04-18 23:43:21 +0900

    read textfiles and add Cartfile

--- ios/KanKenApp/KanKenApp/ViewController/MainViewController.swift
+++ ios/KanKenApp/KanKenApp/ViewController/MainViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Yuichiro Tsuji on 2018/02/23.
 //  Copyright © 2018年 Yuichiro Tsuji. All rights reserved.
 //
+//  iPhone7で実行しないと、ファイルのpath変わります
 
 import UIKit
 
@@ -16,12 +17,31 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let documentDirectoryFileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last {
+            // ディレクトリのパスにファイル名をつなげてファイルのフルパスを作る
+            let targetTextFilePath = documentDirectoryFileURL.appendingPathComponent("test2.txt")
+            print("読み込むファイルのパス: \(targetTextFilePath)")
+            readTextFile(fileURL: targetTextFilePath)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //テキストファイル読み込み処理
+    func readTextFile(fileURL: URL) {
+        do {
+            let text = try String(contentsOf: fileURL, encoding: String.Encoding.utf8)
+            print(text)
+        } catch let error as NSError {
+            print("failed to read: \(error)")
+        }
+    }
+    
     @IBAction func answerTap(_ sender: UIButton) {
         
     }
